00f0fd03ba1e7154a8a41b647b0c07627c7cac96
Jeffrey X <jeffrey.xu@mail.utoronto.ca>
2018-06-24 15:52:48 -0400

    TEAM9-20 added addDoc method for adding indexes to an index

--- searchengineproject/src/main/java/fileIndexing/FileIndexer.java
+++ searchengineproject/src/main/java/fileIndexing/FileIndexer.java
@@ -6,15 +6,20 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.attribute.FileTime;
+
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.StringField;
+import org.apache.lucene.document.TextField;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.index.IndexWriterConfig.OpenMode;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.RAMDirectory;
+import org.apache.lucene.document.Field;
 
 public class FileIndexer {
-  
+
   private static StandardAnalyzer analyzer = null;
   private static Directory index = null;
   private static String docsPath = "file_DIR"; // folder where files to be indexed are stored
@@ -39,33 +44,43 @@ public class FileIndexer {
       System.err.println(e);
     }
   }
-  
+
   /*
-   * Recursively go through each file and folder inside docsPath and add their
-   * attributes to the index
+   * Recursively go through each file and folder inside docsPath and add their attributes to the
+   * index
    */
   private static void indexDocuments(final IndexWriter w, Path currentPath) throws IOException {
     // Directory?
     if (Files.isDirectory(currentPath)) {
-        // Iterate directory
-        for (final File fileEntry : currentPath.toFile().listFiles()) {
-            if (fileEntry.isDirectory()) {
-                indexDocuments(w, fileEntry.toPath());
-            } else {
-                // Index this file
-                addDoc(w, fileEntry.getName(), currentPath, Files.getLastModifiedTime(currentPath));
-            }
+      // Iterate directory
+      for (final File fileEntry : currentPath.toFile().listFiles()) {
+        if (fileEntry.isDirectory()) {
+          indexDocuments(w, fileEntry.toPath());
+        } else {
+          // Index this file
+          addDoc(w, fileEntry.getName(), currentPath, Files.getLastModifiedTime(currentPath));
         }
+      }
     } else {
-        // Index this file
-        addDoc(w, currentPath.getFileName().toString(), currentPath, Files.getLastModifiedTime(currentPath));
+      // Index this file
+      addDoc(w, currentPath.getFileName().toString(), currentPath,
+          Files.getLastModifiedTime(currentPath));
     }
   }
-  
+
   /*
    * Add given attributes to the index: title, path, lastmodified
    */
-  private static void addDoc(IndexWriter w, String title, Path file, FileTime lastModified) throws IOException {
-    
+  private static void addDoc(IndexWriter w, String title, Path file, FileTime lastModified)
+      throws IOException {
+    Document doc = new Document();
+    System.out.println(title + " " + file.toString() + " " + lastModified);
+    doc.add(new TextField("title", title, Field.Store.YES));
+    doc.add(new StringField("path", file.toString(), Field.Store.YES));
+    doc.add(new StringField("modified", lastModified.toString(), Field.Store.YES));
+    // doc.add(new TextField("contents", new String(Files.readAllBytes(file)),
+    // Field.Store.YES));
+    w.addDocument(doc);
+    w.commit();
   }
 }
