2b077901523b922a8ba5f184d472aa1c218b03f0
Jeffrey X <jeffrey.xu@mail.utoronto.ca>
2018-06-24 19:10:28 -0400

    TEAM9-36 writing 2nd unit test to test for reindexing, not complete yet

--- searchengineproject/src/test/java/fileIndexingTests/FileIndexerTest.java
+++ searchengineproject/src/test/java/fileIndexingTests/FileIndexerTest.java
@@ -60,4 +60,46 @@ public class FileIndexerTest {
     }
 
   }
+  
+  @Test
+  public void testNewFileReindexing() {
+    try {
+
+      // this folder gets cleaned up automatically by JUnit
+      File file1 = folder.newFile("test file 1.txt");
+      File file2 = folder.newFile("test file 2.txt");
+      File file3 = folder.newFile("test file 3.txt");
+      File folder1 = folder.newFolder("test folder");
+
+      List<String> expectedTitles = Arrays.asList("test file 1", "test file 2", "test file 3");
+      List<String> expectedTitles2 = Arrays.asList("test file 1", "test file 2", "test file 3", "test file 4");
+      List<String> expectedPath = Arrays.asList(file1.getPath(), file2.getPath(), file3.getPath());
+
+      try {
+
+        MockFrontEnd mockFront = new MockFrontEnd(folder.getRoot().toString(), "test");
+
+        List<String> titleList = mockFront.getTitles();
+        List<String> pathList = mockFront.getPaths();
+        // List<String> modifiedList = mockFront.getModified();
+
+        assertTrue("all titles must be accounted for and correct",
+            titleList.containsAll(expectedTitles));
+        assertTrue("all titles must be accounted for and correct",
+            expectedTitles.containsAll(titleList));
+
+        assertTrue("all paths must be correct", pathList.containsAll(expectedPath));
+        assertTrue("all paths must be accounted for", expectedPath.containsAll(pathList));
+        
+
+      } catch (ParseException e) {
+        fail("Unexpected ParesException occurred");
+        e.printStackTrace();
+      }
+
+    } catch (IOException e) {
+      System.out.println(e);
+    }
+
+  }
 }
