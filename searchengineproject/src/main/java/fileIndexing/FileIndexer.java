c00f41dac2f24c914d2f662381491f8fabb48295
Jeffrey X <jeffrey.xu@mail.utoronto.ca>
2018-06-24 15:48:43 -0400

    TEAM9-20 added indexDocuments method

--- searchengineproject/src/main/java/fileIndexing/FileIndexer.java
+++ searchengineproject/src/main/java/fileIndexing/FileIndexer.java
@@ -1,8 +1,11 @@
 package fileIndexing;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.attribute.FileTime;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
@@ -42,7 +45,27 @@ public class FileIndexer {
    * attributes to the index
    */
   private static void indexDocuments(final IndexWriter w, Path currentPath) throws IOException {
-    
+    // Directory?
+    if (Files.isDirectory(currentPath)) {
+        // Iterate directory
+        for (final File fileEntry : currentPath.toFile().listFiles()) {
+            if (fileEntry.isDirectory()) {
+                indexDocuments(w, fileEntry.toPath());
+            } else {
+                // Index this file
+                addDoc(w, fileEntry.getName(), currentPath, Files.getLastModifiedTime(currentPath));
+            }
+        }
+    } else {
+        // Index this file
+        addDoc(w, currentPath.getFileName().toString(), currentPath, Files.getLastModifiedTime(currentPath));
+    }
+  }
+  
+  /*
+   * Add given attributes to the index: title, path, lastmodified
+   */
+  private static void addDoc(IndexWriter w, String title, Path file, FileTime lastModified) throws IOException {
     
   }
 }
