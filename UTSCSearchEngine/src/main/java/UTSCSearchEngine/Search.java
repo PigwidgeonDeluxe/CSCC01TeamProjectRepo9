24b1740852e5f5ceba8d330dfaaa009dfd13554e
Kevin Zhang <mstr.zhang@mail.utoronto.ca>
2018-06-24 21:00:39 -0400

    integrate file indexer with file search functionality

--- UTSCSearchEngine/src/main/java/UTSCSearchEngine/Search.java
+++ UTSCSearchEngine/src/main/java/UTSCSearchEngine/Search.java
@@ -1,6 +1,10 @@
 package UTSCSearchEngine;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
@@ -32,63 +36,72 @@ public class Search extends HttpServlet {
 
     private static StandardAnalyzer analyzer = null;
     private static Directory index = null;
-    private static final long serialVersionUID = 1L;
-    private FileIndexer fileIndexer = new FileIndexer();
-
-    private static void addDoc(IndexWriter w, String fileName, String fileType,
-                               String userType, String userName) throws IOException {
-        Document doc = new Document();
-
-        doc.add(new TextField("fileName", fileName, Field.Store.YES));
-        doc.add(new TextField("fileType", fileType, Field.Store.YES));
-        doc.add(new TextField("userName", userName, Field.Store.YES));
-        doc.add(new StringField("userType", userType, Field.Store.YES));
-        w.addDocument(doc);
-    }
-
+    private static String docsPath = "./src/main/resources/";
+    private static Path docDir = null;
 
     public void init(ServletConfig config1) throws ServletException {
         super.init(config1);
         System.out.println("Started: init");
-        fileIndexer.doIndexing();
+        doIndexing();
         System.out.println("Finished: init");
     }
 
-
-    private void startIndexing() {
-        /*
-         * 1. analyzer is used for parsing the data out.
-         * the standardAnalyzer will remove the stop words.
-         *
-         * 2. RamDirectory is the indexer that will be maintained in
-         * the RAM
-         */
+    private void doIndexing() {
+        docDir = Paths.get(docsPath);
         analyzer = new StandardAnalyzer();
         index = new RAMDirectory();
-
-        /*
-         * IndexWriterConfig is the configuration that will be used
-         * when creating the config. I am choosing the default one.
-         */
         IndexWriterConfig config = new IndexWriterConfig(analyzer);
-
         try {
-            /*
-             * IndexWriter is used to write into the index.
-             */
+            config.setOpenMode(IndexWriterConfig.OpenMode.CREATE_OR_APPEND);
             IndexWriter w = new IndexWriter(index, config);
-            addDoc(w, "CSCC01 Midterm Solutions.pdf", "PDF","instructor", "abbas");
-            addDoc(w, "CSCC01 study notes.pdf", "PDF","student", "zhangke");
-            addDoc(w, "CSCC01 week 5 lecture notes.docx", "DOCX","student", "zhangke");
+            indexDocuments(w, docDir);
             w.close();
         } catch (IOException e) {
             System.err.println(e);
         }
     }
 
+    private static void indexDocuments(final IndexWriter w, Path currentPath) throws IOException {
+        // Directory?
+        if (Files.isDirectory(currentPath)) {
+            // Iterate directory
+            for (final File fileEntry : currentPath.toFile().listFiles()) {
+                if (fileEntry.isDirectory()) {
+                    indexDocuments(w, fileEntry.toPath());
+                } else {
+                    // Index this file
+                    addDoc(w,
+                            fileEntry.getName(),
+                            fileEntry.getName().substring(fileEntry.getName().lastIndexOf('.') + 1),
+                            "user",
+                            "student");
+                }
+            }
+        } else {
+            // Index this file
+            addDoc(w,
+                    currentPath.getFileName().toString(),
+                    currentPath.getFileName().toString().substring(currentPath.getFileName().toString().lastIndexOf('.') + 1),
+                    "user",
+                    "student");
+        }
+    }
+
+    private static void addDoc(IndexWriter w, String fileName, String fileType, String userName,
+                               String userType) throws IOException {
+        Document doc = new Document();
+
+        doc.add(new TextField("fileName", fileName, Field.Store.YES));
+        doc.add(new TextField("fileType", fileType, Field.Store.YES));
+        doc.add(new TextField("userName", userName, Field.Store.YES));
+        doc.add(new StringField("userType", userType, Field.Store.YES));
+
+        w.addDocument(doc);
+        w.commit();
+    }
+
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-            throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         resp.setContentType("text/plain");
 
         String fileNameQuery = req.getParameter("fileName");
