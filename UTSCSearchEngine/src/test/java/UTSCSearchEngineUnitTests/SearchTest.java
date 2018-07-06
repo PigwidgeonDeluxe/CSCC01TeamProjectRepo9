9b991c8878b9ba4683415b759dfef39b0b29f9a8
Kevin Zhang <mstr.zhang@mail.utoronto.ca>
2018-07-06 11:25:37 -0400

    TEAM9-37 TEAM9-31 TEAM9-32 add test cases for searching by user type

--- UTSCSearchEngine/src/test/java/UTSCSearchEngineUnitTests/SearchTest.java
+++ UTSCSearchEngine/src/test/java/UTSCSearchEngineUnitTests/SearchTest.java
@@ -147,4 +147,39 @@ public class SearchTest {
     Collections.sort(case3List);
     assertEquals("improperly matched files", case3, case3List);
   }
+
+  @Test
+  public void testSearchByUserType() throws NoSuchFieldException, IllegalAccessException,
+      ParseException, IOException {
+
+    File txtFile1 = folder.newFile("test file1.txt");
+    File txtFile2 = folder.newFile("test file2.txt");
+    File pdfFile = folder.newFile("sample file.pdf");
+    File docxFile = folder.newFile("word document.docx");
+    File testFolder = folder.newFolder();
+
+    List<String> testCase = Arrays.asList("sample file.pdf", "test file1.txt", "test file2.txt",
+        "word document.docx");
+
+    Indexing indexer = new Indexing();
+
+    Field field = indexer.getClass().getDeclaredField("docsPath");
+    field.setAccessible(true);
+    field.set(indexer, folder.getRoot().toString());
+    indexer.doIndexing();
+
+    Query q = new QueryParser("userType", indexer.getAnalyzer()).parse("student");
+    IndexReader reader = DirectoryReader.open(indexer.getIndex());
+    IndexSearcher searcher = new IndexSearcher(reader);
+    TopDocs docs = searcher.search(q, 10);
+    ScoreDoc[] hits = docs.scoreDocs;
+    List<String> case1List = new ArrayList<>();
+    for (int i = 0; i < hits.length; ++i) {
+      int docId = hits[i].doc;
+      Document d = searcher.doc(docId);
+      case1List.add(d.get("fileName"));
+    }
+    Collections.sort(case1List);
+    assertEquals("improperly matched files", testCase, case1List);
+  }
 }
