fc82cc7346548d091cf8b0fde68de808aa49a99a
Edgar Lai <edgar.lai@mail.utoronto.ca>
2018-07-22 12:58:48 -0400

    TEAM9-99 finished file download unit test

--- src/main/java/UTSCSearchEngine/FileDownload.java
+++ src/main/java/UTSCSearchEngine/FileDownload.java
@@ -12,25 +12,33 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/download")
 public class FileDownload extends HttpServlet {
 
-  private String docsPath = "./src/main/resources/";
-  private final int BUFFER_LIMIT = 4096;
+	private String docsPath = "./src/main/resources/";
+	private final int BUFFER_LIMIT = 4096;
 
-  @Override
-  public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-    resp.setHeader("Access-Control-Allow-Origin", "*");
-    String fileName = req.getParameter("fileName");
-    resp.setHeader("Content-disposition", "attachment; filename=" + fileName);
+	@Override
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		resp.setHeader("Access-Control-Allow-Origin", "*");
+		String fileName = req.getParameter("fileName");
+		resp.setHeader("Content-disposition", "attachment; filename=" + fileName);
 
-    File file = new File(docsPath + fileName);
+		File file = new File(docsPath + fileName);
 
-    OutputStream out = resp.getOutputStream();
-    FileInputStream in = new FileInputStream(file);
-    byte[] buffer = new byte[BUFFER_LIMIT];
-    int length;
-    while ((length = in.read(buffer)) > 0) {
-      out.write(buffer, 0, length);
-    }
-    in.close();
-    out.flush();
-  }
+		OutputStream out = resp.getOutputStream();
+		FileInputStream in = new FileInputStream(file);
+		byte[] buffer = new byte[BUFFER_LIMIT];
+		int length;
+		while ((length = in.read(buffer)) > 0) {
+			out.write(buffer, 0, length);
+		}
+		in.close();
+		out.flush();
+	}
+
+	public String getDocsPath() {
+		return docsPath;
+	}
+
+	public void setDocsPath(String docsPath) {
+		this.docsPath = docsPath;
+	}
 }
