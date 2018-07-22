07aad4cb651c33ded3326ec016d6f0ce2fd9a19c
Kevin Zhang <mstr.zhang@mail.utoronto.ca>
2018-07-22 19:11:43 -0400

    TEAM9-82 show file content preview in search results

--- src/main/webapp/src/app/navbar/navbar.component.ts
+++ src/main/webapp/src/app/navbar/navbar.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit, TemplateRef } from '@angular/core';
-import { BsModalService } from 'ngx-bootstrap/modal';
 import { BsModalRef } from 'ngx-bootstrap/modal/bs-modal-ref.service';
 
 declare var gapi: any;
