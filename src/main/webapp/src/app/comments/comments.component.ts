9b1191c34c12a85fddc41b3465e7f717bdcd766f
Kevin Zhang <mstr.zhang@mail.utoronto.ca>
2018-07-26 22:13:52 -0400

    TEAM9-90 fix linting errors

--- src/main/webapp/src/app/comments/comments.component.ts
+++ src/main/webapp/src/app/comments/comments.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit} from '@angular/core';
-import {Router, ActivatedRoute, Params} from '@angular/router';
-import * as FileSaver from 'file-saver';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 import swal from 'sweetalert2';
 
 @Component({
@@ -18,7 +17,7 @@ export class CommentsComponent implements OnInit {
 
   docId: string;
   comment: string;
-  comment_user: any;
+  commentUser: any;
 
   constructor(private activatedRoute: ActivatedRoute) { }
 
@@ -26,61 +25,60 @@ export class CommentsComponent implements OnInit {
     this.http = new XMLHttpRequest();
     this.TOMCAT_URL = 'http://localhost:8080';
     this.results = [];
-    this.comment_user = JSON.parse(localStorage.getItem('user'));
+    this.commentUser = JSON.parse(localStorage.getItem('user'));
     this.activatedRoute.queryParams.subscribe((params: Params) => {
         this.docId = params['docId'];
       });
-      console.log("docId:" + this.docId);
-      console.log(this.comment_user)
     this.getComments();
   }
 
-  getComments(){
-  	let url = this.TOMCAT_URL + '/comments?docId=' + this.docId;
-    if (this.docId !== undefined){
-	    this.http.open('GET', url, false);
-	    this.http.send(null);
-	    const resp = this.http.response.split('"\n');
-	    this.results = [];
-	    resp.forEach(element => {
-	      if (element.length > 0) {
-	        this.results.push({
-	          'docId': element.split('~')[0],
-	          'comment': element.split('~')[1],
-	          'comment_user': element.split('~')[2],
-	          'date': element.split('~')[3]
-	        });
-	      }
-	    });
+  getComments() {
+    const url = this.TOMCAT_URL + '/comments?docId=' + this.docId;
+    if (this.docId !== undefined) {
+      this.http.open('GET', url, false);
+      this.http.send(null);
+      const resp = this.http.response.split('"\n');
+      this.results = [];
+      resp.forEach(element => {
+        if (element.length > 0) {
+          this.results.push({
+            'docId': element.split('~')[0],
+            'comment': element.split('~')[1],
+            'commentUser': element.split('~')[2],
+            'date': element.split('~')[3]
+          });
+        }
+      });
     }
     if (this.docId === undefined) {
-  		swal({
-    	title: 'No Document',
-    	type: 'warning',
-    	text: 'No document here'
-  		});
-  	}
+      swal({
+      title: 'No Document',
+      type: 'warning',
+      text: 'No document here'
+      });
+    }
   }
 
-  insertComment(){
-  	if (this.comment !== undefined && this.comment !== ""){
-	  	let url = this.TOMCAT_URL + '/commenting?docId=' + this.docId + "&comment=" + this.comment + "&comment_user=" + this.comment_user.userName;
-	  	this.http.open('POST', url, false);
-	    this.http.send(null);
-	    console.log("comment:" + this.comment);
+  insertComment() {
+    if (this.comment !== undefined && this.comment !== '') {
+      const url = this.TOMCAT_URL + '/commenting?docId=' + this.docId + '&comment=' + this.comment +
+        '&comment_user=' + this.commentUser.userName;
+      this.http.open('POST', url, false);
+      this.http.send(null);
     } else {
-  		swal({
-    	title: 'No Comment',
-    	type: 'warning',
-    	text: 'No comment written'
-  		});
-  	}
+      swal({
+      title: 'No Comment',
+      type: 'warning',
+      text: 'No comment written'
+      });
+    }
     if (this.docId === undefined) {
-  		swal({
-    	title: 'No Document',
-    	type: 'warning',
-    	text: 'No document here'
-  		});
-  	}
-}
+      swal({
+      title: 'No Document',
+      type: 'warning',
+      text: 'No document here'
+      });
+    }
+  }
+
 }
