f895a844bb87a4c75a9ad430920159105d5bb088
Jeffrey X <jeffrey.xu@mail.utoronto.ca>
2018-07-26 22:13:38 -0400

    TEAM9-90 TEAM9-92 fixed formatting errors via tslint

--- src/main/webapp/src/app/comments/comments.component.ts
+++ src/main/webapp/src/app/comments/comments.component.ts
@@ -30,57 +30,56 @@ export class CommentsComponent implements OnInit {
     this.activatedRoute.queryParams.subscribe((params: Params) => {
         this.docId = params['docId'];
       });
-      console.log("docId:" + this.docId);
-      console.log(this.comment_user)
     this.getComments();
   }
 
-  getComments(){
-  	let url = this.TOMCAT_URL + '/comments?docId=' + this.docId;
-    if (this.docId !== undefined){
-	    this.http.open('GET', url, false);
-	    this.http.send(null);
-	    const resp = this.http.response.split('"\n');
-	    this.results = [];
-	    resp.forEach(element => {
-	      if (element.length > 0) {
-	        this.results.push({
-	          'docId': element.split('~')[0],
-	          'comment': element.split('~')[1],
-	          'comment_user': element.split('~')[2],
-	          'date': element.split('~')[3]
-	        });
-	      }
-	    });
+  getComments() {
+    const url = this.TOMCAT_URL + '/comments?docId=' + this.docId;
+    if (this.docId !== undefined) {
+      this.http.open('GET', url, false);
+            this.http.send(null);
+            const resp = this.http.response.split('"\n');
+            this.results = [];
+            resp.forEach(element => {
+              if (element.length > 0) {
+                this.results.push({
+                  'docId': element.split('~')[0],
+                  'comment': element.split('~')[1],
+                  'comment_user': element.split('~')[2],
+                  'date': element.split('~')[3]
+                });
+              }
+            });
     }
     if (this.docId === undefined) {
-  		swal({
-    	title: 'No Document',
-    	type: 'warning',
-    	text: 'No document here'
-  		});
-  	}
+                  swal({
+            title: 'No Document',
+            type: 'warning',
+            text: 'No document here'
+                  });
+          }
   }
 
-  insertComment(){
-  	if (this.comment !== undefined && this.comment !== ""){
-	  	let url = this.TOMCAT_URL + '/commenting?docId=' + this.docId + "&comment=" + this.comment + "&comment_user=" + this.comment_user.userName;
-	  	this.http.open('POST', url, false);
-	    this.http.send(null);
-	    console.log("comment:" + this.comment);
+  insertComment() {
+          if (this.comment !== undefined && this.comment !== '') {
+                  const url = this.TOMCAT_URL + '/commenting?docId=' + this.docId + '&comment=' + this.comment + '&comment_user='
+                  + this.comment_user.userName;
+                  this.http.open('POST', url, false);
+            this.http.send(null);
+            console.log('comment:' + this.comment);
     } else {
-  		swal({
-    	title: 'No Comment',
-    	type: 'warning',
-    	text: 'No comment written'
-  		});
-  	}
+                  swal({
+            title: 'No Comment',
+            type: 'warning',
+            text: 'No comment written'
+                  });
+          }
     if (this.docId === undefined) {
-  		swal({
-    	title: 'No Document',
-    	type: 'warning',
-    	text: 'No document here'
-  		});
-  	}
+                  swal({
+            title: 'No Document',
+            type: 'warning',
+            text: 'No document here'
+                  });
+          }
 }
 }
