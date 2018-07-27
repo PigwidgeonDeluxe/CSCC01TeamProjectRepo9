0472f4a2ab56670a4d612c97d08c3bca57f98c58
Angela Zhu <angelam.zhu@mail.utoronto.ca>
2018-07-27 19:27:48 -0400

    team9-93 added funtionality to store followed users in Database

--- src/main/java/UTSCSearchEngine/Database.java
+++ src/main/java/UTSCSearchEngine/Database.java
@@ -9,128 +9,188 @@ import java.sql.SQLException;
 
 public class Database {
 
-  private String url;
+	private String url;
 
-  public Database() {
-    this.url = "jdbc:sqlite:database.db";
-  }
+	public Database() {
+		this.url = "jdbc:sqlite:database.db";
+	}
 
-  public Database(String url) {
-    this.url = url;
-  }
+	public Database(String url) {
+		this.url = url;
+	}
 
-  public Connection connect() {
+	public Connection connect() {
 
-    Connection con = null;
-    try {
-      con = DriverManager.getConnection(this.url);
-    } catch (SQLException ex) {
-      System.out.println(ex.getMessage());
-    }
+		Connection con = null;
+		try {
+			con = DriverManager.getConnection(this.url);
+		} catch (SQLException ex) {
+			System.out.println(ex.getMessage());
+		}
 
-    return con;
-  }
+		return con;
+	}
 
-  public void insertFileData(byte[] file, String fileName, String fileType, String uploaderName,
-      String uploaderType, Long date) {
+	public void insertFileData(byte[] file, String fileName, String fileType, String uploaderName, String uploaderType,
+			Long date) {
 
-    String sql = "INSERT INTO file(file, file_name, file_type, file_size, uploader_name, "
-        + "uploader_type, uploaded_on) VALUES (?, ?, ?, ?, ?, ?, ?)";
+		String sql = "INSERT INTO file(file, file_name, file_type, file_size, uploader_name, "
+				+ "uploader_type, uploaded_on) VALUES (?, ?, ?, ?, ?, ?, ?)";
 
-    try (Connection con = connect(); PreparedStatement pstmt = con.prepareStatement(sql)) {
-      pstmt.setBytes(1, file);
-      pstmt.setString(2, fileName);
-      pstmt.setString(3, fileType);
-      pstmt.setInt(4, file.length);
-      pstmt.setString(5, uploaderName);
-      pstmt.setString(6, uploaderType);
-      pstmt.setDate(7, date != null ? new Date(date) : new Date(System.currentTimeMillis()));
-      pstmt.executeUpdate();
-    } catch (SQLException ex) {
-      ex.printStackTrace();
-    }
-  }
+		try (Connection con = connect(); PreparedStatement pstmt = con.prepareStatement(sql)) {
+			pstmt.setBytes(1, file);
+			pstmt.setString(2, fileName);
+			pstmt.setString(3, fileType);
+			pstmt.setInt(4, file.length);
+			pstmt.setString(5, uploaderName);
+			pstmt.setString(6, uploaderType);
+			pstmt.setDate(7, date != null ? new Date(date) : new Date(System.currentTimeMillis()));
+			pstmt.executeUpdate();
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+		}
+	}
 
-  public ResultSet getAllFiles() throws SQLException {
+	public ResultSet getAllFiles() throws SQLException {
 
-    String sql = "SELECT * FROM file";
+		String sql = "SELECT * FROM file";
 
-    Connection con = connect();
-    PreparedStatement pstmt = con.prepareStatement(sql);
-    return pstmt.executeQuery();
-  }
+		Connection con = connect();
+		PreparedStatement pstmt = con.prepareStatement(sql);
+		return pstmt.executeQuery();
+	}
 
-  public ResultSet getFileById(String fileId) throws SQLException {
+	public ResultSet getFileById(String fileId) throws SQLException {
 
-    String sql = "SELECT * FROM file WHERE id = ?";
+		String sql = "SELECT * FROM file WHERE id = ?";
 
-    Connection con = connect();
-    PreparedStatement pstmt =  con.prepareStatement(sql);
+		Connection con = connect();
+		PreparedStatement pstmt = con.prepareStatement(sql);
 
-    pstmt.setString(1, fileId);
-    return pstmt.executeQuery();
-  }
+		pstmt.setString(1, fileId);
+		return pstmt.executeQuery();
+	}
 
-  public void insertFileComment(String fileId, String comment, String comment_user, Long date) {
-    String sql =
-        "INSERT INTO comments(file_id, comment, comment_user, date) VALUES (?, ?, ?, ?)";
+	public void insertFileComment(String fileId, String comment, String comment_user, Long date) {
+		String sql = "INSERT INTO comments(file_id, comment, comment_user, date) VALUES (?, ?, ?, ?)";
 
-    try (Connection con = connect(); PreparedStatement pstmt = con.prepareStatement(sql)) {
-      pstmt.setString(1, fileId);
-      pstmt.setString(2, comment);
-      pstmt.setString(3, comment_user);
-      pstmt.setDate(4, date != null ? new Date(date) : new Date(System.currentTimeMillis()));
-      pstmt.executeUpdate();
-    } catch (SQLException ex) {
-      ex.printStackTrace();
-    }
-  }
+		try (Connection con = connect(); PreparedStatement pstmt = con.prepareStatement(sql)) {
+			pstmt.setString(1, fileId);
+			pstmt.setString(2, comment);
+			pstmt.setString(3, comment_user);
+			pstmt.setDate(4, date != null ? new Date(date) : new Date(System.currentTimeMillis()));
+			pstmt.executeUpdate();
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+		}
+	}
 
-  public ResultSet getFileComments(String fileId) throws SQLException {
-    String sql = "SELECT * FROM comments WHERE file_id = ?";
+	public ResultSet getFileComments(String fileId) throws SQLException {
+		String sql = "SELECT * FROM comments WHERE file_id = ?";
 
-    Connection con = connect();
-    PreparedStatement pstmt = con.prepareStatement(sql);
+		Connection con = connect();
+		PreparedStatement pstmt = con.prepareStatement(sql);
 
-    pstmt.setString(1, fileId);
-    return pstmt.executeQuery();
-  }
+		pstmt.setString(1, fileId);
+		return pstmt.executeQuery();
+	}
 
-  public ResultSet getFileData(String fileName, Long uploadTime) throws SQLException {
+	public ResultSet getFileData(String fileName, Long uploadTime) throws SQLException {
 
-    String sql = "SELECT * FROM file WHERE file_name = ? AND uploaded_on = ?";
+		String sql = "SELECT * FROM file WHERE file_name = ? AND uploaded_on = ?";
 
-    Connection con = connect();
-    PreparedStatement pstmt = con.prepareStatement(sql);
+		Connection con = connect();
+		PreparedStatement pstmt = con.prepareStatement(sql);
 
-    pstmt.setString(1, fileName);
-    pstmt.setDate(2, new Date(uploadTime));
-    return pstmt.executeQuery();
-  }
+		pstmt.setString(1, fileName);
+		pstmt.setDate(2, new Date(uploadTime));
+		return pstmt.executeQuery();
+	}
 
-  public void insertUser(String userId, String userType) {
+	public void insertUser(String userId, String userType) {
 
-    String sql = "INSERT INTO user(user_id, user_type, created_on) VALUES(?, ?, ?)";
+		String sql = "INSERT INTO user(user_id, user_type, created_on, follow_num) VALUES(?, ?, ?, ?)";
 
-    try (Connection con = connect(); PreparedStatement pstmt = con.prepareStatement(sql)) {
-      pstmt.setString(1, userId);
-      pstmt.setString(2, userType);
-      pstmt.setDate(3, new Date(System.currentTimeMillis()));
-      pstmt.executeUpdate();
-    } catch (SQLException ex) {
-      ex.printStackTrace();
-    }
-  }
+		try (Connection con = connect(); PreparedStatement pstmt = con.prepareStatement(sql)) {
+			pstmt.setString(1, userId);
+			pstmt.setString(2, userType);
+			pstmt.setDate(3, new Date(System.currentTimeMillis()));
+			pstmt.setInt(4, 0);
+			pstmt.executeUpdate();
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+		}
+	}
 
-  public ResultSet getUserById(String userId) throws SQLException {
+	public ResultSet getUserById(String userId) throws SQLException {
 
-    String sql = "SELECT * FROM user WHERE user_id = ?";
+		String sql = "SELECT * FROM user WHERE user_id = ?";
 
-    Connection con = connect();
-    PreparedStatement pstmt = con.prepareStatement(sql);
+		Connection con = connect();
+		PreparedStatement pstmt = con.prepareStatement(sql);
 
-    pstmt.setString(1, userId);
-    return pstmt.executeQuery();
-  }
+		pstmt.setString(1, userId);
+		return pstmt.executeQuery();
+	}
 
+	public void insertUserFollow(String userId, String userIdFollow) {
+
+		String sql = "INSERT INTO follow(user_id, follow_id) VALUES (?, ?)";
+
+		try (Connection con = connect(); PreparedStatement pstmt = con.prepareStatement(sql)) {
+			pstmt.setString(1, userId);
+			pstmt.setString(2, userIdFollow);
+			pstmt.executeUpdate();
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+		}
+	}
+
+	public void updateUserFollow(String userId, int followNum) {
+
+		String sql = "UPDATE users SET follow_num = ? WHERE user_id = ?";
+
+		try (Connection con = connect(); PreparedStatement pstmt = con.prepareStatement(sql)) {
+			pstmt.setInt(1, followNum);
+			pstmt.setString(2, userId);
+			pstmt.executeUpdate();
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+		}
+	}
+
+	public ResultSet getUserFollow(String userId) throws SQLException {
+
+		String sql = "SELECT * FROM comments WHERE user_id = ?";
+
+		Connection con = connect();
+		PreparedStatement pstmt = con.prepareStatement(sql);
+
+		pstmt.setString(1, userId);
+		return pstmt.executeQuery();
+	}
+
+	public void insertUserUpdate(String userId, String fileId) {
+
+		String sql = "INSERT INTO update(user_id, file_id) VALUES (?, ?)";
+
+		try (Connection con = connect(); PreparedStatement pstmt = con.prepareStatement(sql)) {
+			pstmt.setString(1, userId);
+			pstmt.setString(2, fileId);
+			pstmt.executeUpdate();
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+		}
+	}
+
+	public ResultSet getUserUpdate(String userId) throws SQLException {
+
+		String sql = "SELECT * FROM comments WHERE user_id = ?";
+
+		Connection con = connect();
+		PreparedStatement pstmt = con.prepareStatement(sql);
+
+		pstmt.setString(1, userId);
+		return pstmt.executeQuery();
+	}
 }
---
+++ src/main/java/UTSCSearchEngine/Follow.java
@@ -0,0 +1,71 @@
+package UTSCSearchEngine;
+
+import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONObject;
+
+/**
+ * Class for Retrieving Followed User Updates
+ *
+ */
+@WebServlet("/follow")
+public class Follow extends HttpServlet {
+
+	@Override
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		resp.setContentType("multipart/form-data");
+		resp.setHeader("Access-Control-Allow-Origin", "*");
+
+		String userId = req.getParameter("userId");
+		StringBuilder responseBackToUser = new StringBuilder();
+
+		Database db = new Database();
+		JSONObject response = new JSONObject();
+
+		// package user follower data
+		try {
+			ResultSet user = db.getUserFollow(userId);
+			while (user.next()) {
+				String userIdFollow = user.getString("follow_id");
+				ResultSet update = db.getUserUpdate(userIdFollow);
+				responseBackToUser.append(update.getString("user_id") + "~" + update.getString("file_id") + "\n");
+			}
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+		}
+
+		// write response back to user
+		resp.getWriter().write(response.toString());
+	}
+
+	@Override
+	public void doPost(HttpServletRequest req, HttpServletResponse resp) {
+		resp.setContentType("multipart/form-data");
+		resp.setHeader("Access-Control-Allow-Origin", "*");
+
+		String userId = req.getParameter("userId");
+		String userIdFollow = req.getParameter("userIdFollow");
+
+		Database db = new Database();
+
+		int followNum = 0;
+		try {
+			ResultSet user = db.getUserById(userId);
+			while (user.next()) {
+				followNum = user.getInt("follow_num");
+			}
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+		}
+
+		db.insertUserFollow(userId, userIdFollow);
+		db.updateUserFollow(userId, followNum);
+	}
+}
