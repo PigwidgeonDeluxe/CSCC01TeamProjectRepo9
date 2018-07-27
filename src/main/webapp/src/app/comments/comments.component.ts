999359433fb6a3d306627f13b78cc320615a7b42
Jeffrey X <jeffrey.xu@mail.utoronto.ca>
2018-07-26 21:36:10 -0400

    TEAM9-90 TEAM9-92 added warning and prevention for commenting when commenting on no document, or undefined comment

--- src/main/webapp/src/app/comments/comments.component.ts
+++ src/main/webapp/src/app/comments/comments.component.ts
@@ -37,28 +37,51 @@ export class CommentsComponent implements OnInit {
 
   getComments(){
   	let url = this.TOMCAT_URL + '/comments?docId=' + this.docId;
-    
-    this.http.open('GET', url, false);
-    this.http.send(null);
-    const resp = this.http.response.split('"\n');
-    this.results = [];
-    resp.forEach(element => {
-      if (element.length > 0) {
-        this.results.push({
-          'docId': element.split('~')[0],
-          'comment': element.split('~')[1],
-          'comment_user': element.split('~')[2],
-          'date': element.split('~')[3]
-        });
-      }
-    });
-    console.log("getting comments");
+    if (this.docId !== undefined){
+	    this.http.open('GET', url, false);
+	    this.http.send(null);
+	    const resp = this.http.response.split('"\n');
+	    this.results = [];
+	    resp.forEach(element => {
+	      if (element.length > 0) {
+	        this.results.push({
+	          'docId': element.split('~')[0],
+	          'comment': element.split('~')[1],
+	          'comment_user': element.split('~')[2],
+	          'date': element.split('~')[3]
+	        });
+	      }
+	    });
+    }
+    if (this.docId === undefined) {
+  		swal({
+    	title: 'No Document',
+    	type: 'warning',
+    	text: 'No document here'
+  		});
+  	}
   }
 
   insertComment(){
-  	let url = this.TOMCAT_URL + '/commenting?docId=' + this.docId + "&comment=" + this.comment + "&comment_user=" + this.comment_user.userName;
-  	this.http.open('POST', url, false);
-    this.http.send(null);
-    console.log("comment:" + this.comment);
-  }
+  	if (this.comment !== undefined){
+	  	let url = this.TOMCAT_URL + '/commenting?docId=' + this.docId + "&comment=" + this.comment + "&comment_user=" + this.comment_user.userName;
+	  	this.http.open('POST', url, false);
+	    this.http.send(null);
+	    console.log("comment:" + this.comment);
+    }
+
+    if (this.comment === undefined) {
+  		swal({
+    	title: 'No Comment',
+    	type: 'warning',
+    	text: 'No comment written'
+  		});
+  	}
+    if (this.docId === undefined) {
+  		swal({
+    	title: 'No Document',
+    	type: 'warning',
+    	text: 'No document here'
+  		});
+  	}
 }
